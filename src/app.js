b6b06d98b87b829565e4d0bc1d1b6c3704310edc
yossipoli <yossipoli@gmail.com>
2022-08-02 15:18:51 +0300

    connected all

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import { is4Digits } from "./clients/prime4Digits.js";
 import PrimeGenerator from "./publisher/primeGenerator.js";
 
 const run = (limit = 25) => {
+    const generator = new PrimeGenerator(limit)
+    generator.on("new", (num)=> isMersenne(num))
+    generator.on("new", (num)=> is4Digits(num))
+    generator.on("new", (num)=> isEnd1(num))
 
+    generator.generate(limit)
 }
 
 export default run
